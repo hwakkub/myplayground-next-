68848dfd3f61f94a60b37668f1ff4af5f480c24e
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-02-13 22:45:30 +0700

    link

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const About = () => {
+  return (
+    <>
+      <p>This is about page</p>
+      <Link href="/">Back to Home</Link>
+    </>
+  )
+}
+
+export default About
