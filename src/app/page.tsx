701d43212e2fdf8eef5f7eb58f2f26e57ba88e94
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-02-25 13:34:50 +0700

    form(2)_ inpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-"use client";
+// "use client";
 import Link from 'next/link'
 import React, { useState } from 'react'
 import {Form} from "./form/page"
+import { revalidatePath } from 'next/cache';
 
 type Props = {};
-export default function Home({}:Props) {
+export default async function Home({}:Props) {
   // const [count, setCount] = useState(0);
 
   // function handleIncrement() {
   //   setCount(count + 1);
   // }
-
+  const result = await fetch("http://localhost:3000/api/todo");
+  const todoList: {id: string; message: string}[] = await result.json();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <Link href="/about">Go to about</Link>
@@ -22,14 +24,31 @@ export default function Home({}:Props) {
       <button onClick={handleIncrement}>Increment</button> */}
        {/* <Form></Form> */}
       <h1>Todos</h1>
-      <form>
+      <form action={async(formData:FormData)=>{
+        "use server";
+        console.log("run server")
+        const message = formData.get("message");
+        await fetch("http://localhost:3000/api/todo",{
+          method:"POST",
+          headers:{"Content-Type":"application/json"},
+          body:JSON.stringify({message}),
+        });
+        revalidatePath("/");
+      }}>
         <input
           type='text'
           name='message'
           className='px-4 py-2 mb-3'
           placeholder='write your job...'/>
-        <button type='submit'>Subnit</button>
+        <button type='submit'>Submit</button>
       </form>
+      <ul>
+        {todoList.map((todo)=>(
+          <li key={todo.id}>
+            Job {todo.id}: {todo.message}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
