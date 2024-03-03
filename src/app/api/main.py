a5e885415b509ef1b4094ef4070a78ff02489fab
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-03-03 15:52:18 +0700

    page not found & params

---
+++ src/app/api/main.py
@@ -0,0 +1,33 @@
+from typing import Optional, Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    username: str
+    password: str
+    level: Optional[str] = "normal"
+
+# http://127.0.0.1:3000/docs
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/hi")
+def hi(name:str):
+    return {"hi": name}
+
+# http://127.0.0.1:3000/name?name=Hwak&surname=Jira
+@app.get("/name")
+def hi(name:str, surname: Optional[str] = None):
+    return {"nickname": name,"surname":surname}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/login")
+def login(user:User):
+    return {"echo":user}
