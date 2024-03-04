a984bffe7d72ff28134b6ff1b97c76b057efb15b
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-03-04 17:16:26 +0700

    useState

--- src/app/(auth)/(with-auth-layout)/layout.tsx
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -2,21 +2,26 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import "./styles.css";
+import { useState } from "react";
 const navLinks = [
   { name: "Register", href: "/register" },
   { name: "Login", href: "/login" },
   { name: "Forgot Password", href: "/forgot-password" },
 ];
-
+// template.tsx ทำได้เหมือนกันแต่ข้อมูลตรง useState จะไม่ไปด้วย
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
+  const [input, setInput] = useState("");
 
   return (
     <div>
+      <div>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+      </div>
       <h2>Inner layout</h2>
       {navLinks.map((link) => {
         const isActive = pathname.startsWith(link.href);
