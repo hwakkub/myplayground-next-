c57fe095f4e0c313f41f18db66ab6161416a2683
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-03-04 13:14:07 +0700

    metadata + params

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,23 @@
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${params.productId}`,
+  };
+};
 export default function ProductId({
   params,
 }: {
