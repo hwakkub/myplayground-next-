5373624f01148f2c14ccae7a1d3be3b8c97801ef
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-03-04 12:46:59 +0700

    metadata title

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -1,16 +1,21 @@
-import Link from 'next/link'
+import Link from "next/link";
 
+export const metadata = {
+  title: "About data",
+};
 export default function Data({
-    searchParams,
-}:{
-    searchParams:{
-        name: string;
-        age: string;
-    };
+  searchParams,
+}: {
+  searchParams: {
+    name: string;
+    age: string;
+  };
 }) {
-    return <>
-     <p>This is data page</p>
-     <p>{searchParams.name}</p>
-     <Link href="/">Back to Home</Link>
+  return (
+    <>
+      <p>This is data page</p>
+      <p>{searchParams.name}</p>
+      <Link href="/">Back to Home</Link>
     </>
+  );
 }
