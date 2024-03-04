b086bfd7cae7b806b8c2e842f5456f9ee0ce5356
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-03-04 16:07:05 +0700

    Active link

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+import { useRouter } from "next/navigation";
 export default function Product() {
-  return <h1>Hi Product</h1>;
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/");
+    // router.replace("/");
+    // router.back();
+    // router.forward();
+  };
+  return (
+    <>
+      <h1>Hi Product</h1>
+      <button onClick={handleClick}>Place order</button>
+    </>
+  );
 }
