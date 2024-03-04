908794742025236ef1dcb17838b819392ddc9dfb
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-03-04 15:34:55 +0700

    links in const

--- src/app/(auth)/(with-auth-layout)/layout.tsx
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
+const navLinks = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot Password", href: "/forgot-password" },
+];
+
 export default function AuthLayout({
   children,
 }: {
@@ -6,6 +14,13 @@ export default function AuthLayout({
   return (
     <div>
       <h2>Inner layout</h2>
+      {navLinks.map((link) => {
+        return (
+          <Link href={link.href} key={link.name}>
+            {link.name}
+          </Link>
+        );
+      })}
       {children}
     </div>
   );
